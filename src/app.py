750ccdb53a74cde4cb3944c4151d8be6dc7414b8
Emma Lister <lisemmak@amazon.com>
2025-05-27 15:53:00 +0100

    cors

--- src/app.py
+++ src/app.py
@@ -14,7 +14,7 @@ from .modules.annotation_table import (
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, resources={r"/api/*": {"origins": "https://annotationtasktrackerfrontend.onrender.com"}})
 
 
 @app.route("/log_in", methods=["POST"])
